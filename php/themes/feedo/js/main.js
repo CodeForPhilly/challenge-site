2d1e5203aad312620d357efb3fb6ec06b8c37e04
Lloyd Emelle <lloyd@emelle.me>
2014-10-08 04:38:25 -0400

    updates

--- php/themes/feedo/js/main.js
+++ php/themes/feedo/js/main.js
@@ -99,12 +99,12 @@ $.backstretch("themes/feedo/img/bg01.jpg", {speed: 500});
 })(jQuery)
 
 ;(function(){
-    $(".slider").noUiSlider({
+    /*$(".slider").noUiSlider({
         start: 40,
         connect: "lower",
         range: {
             min: 0,
             max: 100
         }
-    });
+    });*/
 })(jQuery)
